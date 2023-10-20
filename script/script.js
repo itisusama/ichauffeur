13bc0e79f1f1ebd6bf7e5cf17a46096a376ec4fb
Usama Hassan <ahmarkhan1233456@gmail.com>
2023-10-20 02:54:24 -0700

    form on click

--- script/script.js
+++ script/script.js
@@ -8,3 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   
+  document.querySelector('.by-the-hour').style.display = 'none';
+  function showOneWay() {
+    document.querySelector('.one-way').style.display = 'block';
+    document.querySelector('.by-the-hour').style.display = 'none';
+}
+
+function showByTheHour() {
+    document.querySelector('.by-the-hour').style.display = 'block';
+    document.querySelector('.one-way').style.display = 'none';
+}
