1287b8d5a970111a46e54e43880a399f53d096cf
Usama Hassan <ahmarkhan1233456@gmail.com>
2023-10-21 05:03:38 -0700

    our services and slider

--- script/script.js
+++ script/script.js
@@ -18,3 +18,32 @@ function showByTheHour() {
     document.querySelector('.by-the-hour').style.display = 'block';
     document.querySelector('.one-way').style.display = 'none';
 }
+
+// Slider
+let slideIndex = 0;
+
+function moveSlider(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    const slides = document.querySelectorAll(".slide");
+    const indicators = document.querySelectorAll(".indicator");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        indicators[i].classList.remove("active");
+    }
+
+    slides[slideIndex].style.display = "block";
+    indicators[slideIndex].classList.add("active");
+}
+
+showSlide(slideIndex);
