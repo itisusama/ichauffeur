33ef681a32e96afdd4feda6893f3ab15d6877761
Usama Hassan <ahmarkhan1233456@gmail.com>
2023-10-23 04:04:04 -0700

    mobile-menu added

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.getElementById('nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
+    const navMenu = document.querySelector('.mobile-menu');
   
     navToggle.addEventListener('click', function () {
       navMenu.classList.toggle('active');
       document.getElementById('navbar').classList.toggle('nav-toggle-color');
     });
+  });
+  
+// mobile menu
+  const navToggle = document.getElementById('nav-toggle');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  navToggle.addEventListener('click', function() {
+    if (mobileMenu.style.display === 'block') {
+      mobileMenu.style.display = 'none';
+    } else {
+      mobileMenu.style.display = 'block';
+    }
   });
   
   document.querySelector('.by-the-hour').style.display = 'none';
